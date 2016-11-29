11ce9b719f0d7f6f700c91752c5686f09d891376
Benjamin Li <benji102491@gmail.com>
2016-11-29 17:32:50 -0500

    added characteristics of a puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#Species ----------------------
+#Homo Scriptor Optimus Amicus 
+
+#Characteristics --------------
+#Color : Light Brown
+#Fluffiness : EXTREME
+#Ears : Short 
+#Tail : Long, Fluffy
+#Snout : Thin, Medium-length
+
+#Behavior ---------------------
+#Loud
+#Territorial
+#Attention-seeking
